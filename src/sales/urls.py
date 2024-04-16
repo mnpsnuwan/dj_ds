e3e8fcb789dcbc4f3aa0179305d84b427c70a539
mnpsnuwan <nuwanp124@gmail.com>
2024-04-16 00:02:28 -0700

    IM:DEV:DS Reports - Working on the sales list.

--- src/sales/urls.py
+++ src/sales/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import home_view
+from .views import (
+    home_view,
+    SaleListView,
+)
 
 app_name = 'sales'
 
 urlpatterns = [
     path('', home_view, name='home'),
+    path('list/', SaleListView.as_view(), name='list'),
 ]
--- src/sales/views.py
+++ src/sales/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Sale
 
 # Create your views here.
 
 def home_view(request):
     hello = 'Hello World from the view!'
-    return render(request, 'sales/main.html', {'hello':hello})
+    return render(request, 'sales/home.html', {'hello':hello})
+
+class SaleListView(ListView):
+    model = Sale
+    template_name = 'sales/main.html'
+    context_object_name = 'qs'
+
  
