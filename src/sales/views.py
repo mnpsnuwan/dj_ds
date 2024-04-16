449fe6116b77a02114684ecb2bc1c0cf570d306c
mnpsnuwan <nuwanp124@gmail.com>
2024-04-15 19:37:08 -0700

    IM:DEV:DS Reports - Creating first view and template.

--- src/sales/views.py
+++ src/sales/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home_view(request):
+    hello = 'Hello World from the view!'
+    return render(request, 'sales/main.html', {'hello':hello})
+ 
