c86a256726646736f816c5b7ed9d41c53553bcaa
mnpsnuwan <nuwanp124@gmail.com>
2024-04-24 10:41:54 -0700

    IM:DEV:DS Reports - Add the chart to the modal.

--- src/static/sales/home.js
+++ src/static/sales/home.js
@@ -2,6 +2,7 @@ console.log("Hello World!")
 
 const reportBtn = document.getElementById('report-btn')
 const img = document.getElementById('img')
+const modalBody = document.getElementById('modal-body')
 
 console.log(reportBtn)
 console.log(img)
@@ -9,3 +10,9 @@ console.log(img)
 if (img) {
     reportBtn.classList.remove('not-visible')
 }
+
+reportBtn.addEventListener('click', ()=>{
+    console.log('clicked')
+    img.setAttribute('class', 'w-100')
+    modalBody.prepend(img)
+})
