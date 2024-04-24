4ff921fdb0939e97a315e2229e6f585b6488449b
mnpsnuwan <nuwanp124@gmail.com>
2024-04-24 13:52:55 -0700

    IM:DEV:DS Reports - Adding alerts to the modal.

--- src/sales/static/sales/home.js
+++ src/sales/static/sales/home.js
@@ -4,17 +4,25 @@ const reportBtn = document.getElementById('report-btn')
 const img = document.getElementById('img')
 const modalBody = document.getElementById('modal-body')
 const reportForm = document.getElementById('report-form')
+const alertBox = document.getElementById('alert-box')
 
 const reportName = document.getElementById('id_name')
 const reportRemarks = document.getElementById('id_remarks')
 const csrf = document.getElementsByName('csrfmiddlewaretoken')[0].value
 
-console.log(reportName)
-console.log(reportRemarks)
-console.log(csrf)
+const handleAlerts = (type, msg) => {
+    alertBox.innerHTML =  
+        `<div class="alert alert-${type}" role="alert">
+            ${msg}
+        </div>`
+}
 
-console.log(reportBtn)
-console.log(img)
+// console.log(reportName)
+// console.log(reportRemarks)
+// console.log(csrf)
+
+// console.log(reportBtn)
+// console.log(img)
 
 if (img) {
     reportBtn.classList.remove('not-visible')
@@ -31,8 +39,8 @@ reportBtn.addEventListener('click', ()=>{
         e.preventDefault()
         const formData = new FormData()
         formData.append('csrfmiddlewaretoken', csrf)
-        formData.append('name', reportName)
-        formData.append('remarks', reportRemarks)
+        formData.append('name', reportName.value)
+        formData.append('remarks', reportRemarks.value)
         formData.append('image', img.src)
 
         $.ajax({
@@ -41,9 +49,11 @@ reportBtn.addEventListener('click', ()=>{
             data: formData,
             success: function(response){
                 console.log(response)
+                handleAlerts('success', 'Report created.')
             },
             error: function(error){
                 console.log(error)
+                handleAlerts('danger', 'Ups... someting went wrong.')
             },
             processData: false,
             contentType: false,
