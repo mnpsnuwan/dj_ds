b139a76be3d06755da24be7d796d2bbc4cc123d8
mnpsnuwan <nuwanp124@gmail.com>
2024-04-25 21:39:03 -0700

    IM:DEV:DS Reports - Adding my profile.

--- src/profiles/views.py
+++ src/profiles/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Profile
+from .forms import ProfileForm
 
 # Create your views here.
+
+def my_profile_view(request):
+    context = {}
+    return render(request, 'profiles/main.html', context)
--- src/reports_proj/urls.py
+++ src/reports_proj/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('sales.urls', namespace='sales')),
     path('reports/', include('reports.urls', namespace='reports')),
+    path('my-profile/', include('profiles.urls', namespace='profiles')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
