dcac07085c6d580cea3ce19ffa62cfb4007027f0
mnpsnuwan <nuwanp124@gmail.com>
2024-04-25 22:07:21 -0700

    IM:DEV:DS Reports - Working on my profile.

--- src/profiles/views.py
+++ src/profiles/views.py
@@ -5,5 +5,17 @@ from .forms import ProfileForm
 # Create your views here.
 
 def my_profile_view(request):
-    context = {}
+    profile = Profile.objects.get(user=request.user)
+    form = ProfileForm(request.POST or None, request.FILES or None, instance=profile)
+    confirm = False
+
+    if form.is_valid():
+        form.save()
+        confirm = True
+
+    context = {
+        'profile': profile,
+        'form': form,
+        'confirm': confirm,
+    }
     return render(request, 'profiles/main.html', context)
