416cabfa46e97167d2b0bba145ec659dcbb2372f
mnpsnuwan <nuwanp124@gmail.com>
2024-04-25 17:31:03 -0700

    IM:DEV:DS Reports - Improving the csv upload method.

--- src/reports/views.py
+++ src/reports/views.py
@@ -34,32 +34,36 @@ class UploadTemplateView(TemplateView):
 def csv_upload_view(request):
     print('file is being')
     if request.method == 'POST':
+        csv_file_name = request.FILES.get('file').name
         csv_file = request.FILES.get('file')
-        obj = CSV.objects.create(file_name=csv_file)
+        obj, created = CSV.objects.get_or_create(file_name=csv_file_name)
 
-        with open(obj.file_name.path, 'r') as f:
-            reader = csv.reader(f)
-            reader.__next__()
-            for row in reader:
-                transaction_id = row[1]
-                product = row[2]
-                quantity = int(row[3])
-                customer = row[4]
-                date = parse_date(row[5])
+        if created:
+            obj.csv_file = csv_file
+            obj.save()
+            with open(obj.csv_file.path, 'r') as f:
+                reader = csv.reader(f)
+                reader.__next__()
+                for row in reader:
+                    transaction_id = row[1]
+                    product = row[2]
+                    quantity = int(row[3])
+                    customer = row[4]
+                    date = parse_date(row[5])
 
-                try:
-                    product_obj = Product.objects.get(name__iexact=product)
-                except Product.DoesNotExist:
-                    product_obj = None
+                    try:
+                        product_obj = Product.objects.get(name__iexact=product)
+                    except Product.DoesNotExist:
+                        product_obj = None
 
-                if product_obj is not None:
-                    customer_obj, _ = Customer.objects.get_or_create(name=customer)
-                    salesman_obj = Profile.objects.get(user=request.user)
-                    position_obj = Position.objects.create(product=product_obj, quantity=quantity, created=date)
+                    if product_obj is not None:
+                        customer_obj, _ = Customer.objects.get_or_create(name=customer)
+                        salesman_obj = Profile.objects.get(user=request.user)
+                        position_obj = Position.objects.create(product=product_obj, quantity=quantity, created=date)
 
-                    sale_obj, _ = Sale.objects.get_or_create(transaction_id=transaction_id, customer=customer_obj, salesman=salesman_obj, created=date)
-                    sale_obj.positions.add(position_obj)
-                    sale_obj.save()
+                        sale_obj, _ = Sale.objects.get_or_create(transaction_id=transaction_id, customer=customer_obj, salesman=salesman_obj, created=date)
+                        sale_obj.positions.add(position_obj)
+                        sale_obj.save()
 
     return HttpResponse()
 
--- src/sales/models.py
+++ src/sales/models.py
@@ -51,8 +51,8 @@ class Sale(models.Model):
         return self.positions.all()
 
 class CSV(models.Model):
-    file_name = models.FileField(upload_to='csvs')
-    activated = models.BooleanField(default=False)
+    file_name = models.CharField(max_length=120, null=True)
+    csv_file = models.FileField(upload_to='csvs', null=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
